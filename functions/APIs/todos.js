36a3de9546bc51bb711f12a47f1d20cdd1559e58
Victor Mindrila <victormindrila@gmail.com>
2020-07-07 21:44:50 +0300

    adds todos api

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,15 +1,26 @@
-exports.getAllTodos = (request, response) => {
-	todos = [
-		{
-			id: '1',
-			title: 'greeting',
-			body: 'Hello world'
-		},
-		{
-			id: '2',
-			title: 'greeting2',
-			body: 'Hello2 world2'
-		}
-	];
-	return response.json(todos);
+const { db } = require('../util/admin');
+
+exports.getAllTodos = async (request, response) => {
+	try {
+		const todosSnapshots = await db.collection('todos').orderBy('createdAt', 'desc').get();
+
+		let todos = [];
+
+		todosSnapshots.forEach((doc) => {
+			todos.push({
+				todoId: doc.id,
+				title: doc.data().title,
+				body: doc.data().body,
+				createdAt: doc.data().createdAt
+			});
+		});
+		return response.json(todos);
+	} catch (error) {
+		console.error(error);
+		return response.status(500).json({ error: error.code });
+	}
 };
+
+exports.addOneTodo = async (request, response) => {};
+
+exports.getOneTodo = async (request, response) => {};
