75c5c8c5e418da4d4707a658e3a863a3c656a13c
Victor Mindrila <victormindrila@gmail.com>
2020-07-08 15:34:43 +0300

    adds auth middleware to todos

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -2,13 +2,14 @@ const { db } = require('../util/admin');
 
 exports.getAllTodos = async (request, response) => {
 	try {
-		const todosSnapshots = await db.collection('todos').orderBy('createdAt', 'desc').get();
+		const todosSnapshots = await db.collection('todos').where('username', '==', request.user.username).get();
 
 		let todos = [];
 
 		todosSnapshots.forEach((doc) => {
 			todos.push({
 				todoId: doc.id,
+				username: doc.data().username,
 				title: doc.data().title,
 				body: doc.data().body,
 				createdAt: doc.data().createdAt
@@ -23,11 +24,12 @@ exports.getAllTodos = async (request, response) => {
 
 exports.addOneTodo = (request, response) => {
 	const { title, date, status } = request.body;
+	const { username } = request.user;
 	db
 		.collection('todos')
-		.add({ title, date, status })
+		.add({ username, title, date, status })
 		.then((doc) => {
-			const responseTodoItem = { title, date, status };
+			const responseTodoItem = { username, title, date, status };
 			responseTodoItem.id = doc.id;
 			return response.json(responseTodoItem);
 		})
--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,13 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+const auth = require('./util/auth');
 const { getAllTodos, addOneTodo } = require('./APIs/todos');
 const { signin, signup } = require('./APIs/users');
 
 //todos
-app.get('/todos', getAllTodos);
-app.post('/todos/add', addOneTodo);
+app.get('/todos', auth, getAllTodos);
+app.post('/todos/add', auth, addOneTodo);
 
 // users
 app.post('/signin', signin);
